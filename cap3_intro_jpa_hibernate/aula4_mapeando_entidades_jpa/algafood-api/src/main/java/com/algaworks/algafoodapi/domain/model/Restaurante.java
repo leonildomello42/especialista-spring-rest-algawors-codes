aaa8e94d8351400e1a557cb92526616f5e0e633b
leonildomello42 <leonildo.mello@gmail.com>
2021-05-10 13:23:10 -0300

    Mapeando entidades com JPA

---
+++ cap3_intro_jpa_hibernate/aula4_mapeando_entidades_jpa/algafood-api/src/main/java/com/algaworks/algafoodapi/domain/model/Restaurante.java
@@ -0,0 +1,62 @@
+package com.algaworks.algafoodapi.domain.model;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity
+@Table
+public class Restaurante implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private Long id;
+
+    private String nome;
+
+    @Column(name = "taxa_Frete")
+    private BigDecimal taxaFrete;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getTaxaFrete() {
+        return taxaFrete;
+    }
+
+    public void setTaxaFrete(BigDecimal taxaFrete) {
+        this.taxaFrete = taxaFrete;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Restaurante that = (Restaurante) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
