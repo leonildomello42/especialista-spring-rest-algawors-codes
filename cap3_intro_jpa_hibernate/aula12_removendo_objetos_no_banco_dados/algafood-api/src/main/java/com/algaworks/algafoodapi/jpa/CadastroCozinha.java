076c6b6c41fd41c0accc3dd04f483f9c412ff1bb
leonildomello42 <leonildo.mello@gmail.com>
2021-05-10 21:13:06 -0300

    CRUD cozinha teste

---
+++ cap3_intro_jpa_hibernate/aula12_removendo_objetos_no_banco_dados/algafood-api/src/main/java/com/algaworks/algafoodapi/jpa/CadastroCozinha.java
@@ -0,0 +1,52 @@
+package com.algaworks.algafoodapi.jpa;
+
+import com.algaworks.algafoodapi.domain.model.Cozinha;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Component
+public class CadastroCozinha {
+
+    @PersistenceContext
+    public EntityManager manager;
+
+    public List<Cozinha> listar(){
+
+        TypedQuery<Cozinha> query = manager.createQuery("from Cozinha", Cozinha.class);
+        List<Cozinha> cozinhas = query.getResultList();
+
+        return cozinhas;
+    }
+
+    public Cozinha buscar(Long id){
+        return manager.find(Cozinha.class, id);
+    }
+
+    @Transactional
+    public Cozinha salvar(Cozinha cozinha){
+
+        Cozinha cozinhaSalva = manager.merge(cozinha);
+        return cozinhaSalva;
+    }
+
+    @Transactional
+    public Cozinha atualizar(String nome, Long id){
+
+        Cozinha cozinha1 = buscar(id);
+        cozinha1.setNome(nome);
+
+        return manager.merge(cozinha1);
+    }
+
+    @Transactional
+    public void remover (Cozinha cozinha){
+
+        cozinha = buscar(cozinha.getId());
+        manager.remove(cozinha);
+    }
+}
