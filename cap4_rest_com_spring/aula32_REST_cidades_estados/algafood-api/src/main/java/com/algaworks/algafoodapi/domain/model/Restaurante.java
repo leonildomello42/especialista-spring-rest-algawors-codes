17840d5590ba57dc6c22a4b8f146cc21a65a5475
leonildomello42 <leonildo.mello@gmail.com>
2021-05-13 23:12:42 -0300

    REST de estados e cidades

---
+++ cap4_rest_com_spring/aula32_REST_cidades_estados/algafood-api/src/main/java/com/algaworks/algafoodapi/domain/model/Restaurante.java
@@ -0,0 +1,73 @@
+package com.algaworks.algafoodapi.domain.model;
+
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity
+@Table
+public class Restaurante implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @ManyToOne()
+    @JoinColumn(name = "cozinha_id", nullable = false) // nome da coluna
+    private Cozinha cozinha;
+
+    @Column(name = "taxa_Frete")
+    private BigDecimal taxaFrete;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getTaxaFrete() {
+        return taxaFrete;
+    }
+
+    public void setTaxaFrete(BigDecimal taxaFrete) {
+        this.taxaFrete = taxaFrete;
+    }
+
+    public Cozinha getCozinha() {
+        return cozinha;
+    }
+
+    public void setCozinha(Cozinha cozinha) {
+        this.cozinha = cozinha;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Restaurante that = (Restaurante) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
