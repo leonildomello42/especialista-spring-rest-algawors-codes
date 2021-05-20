f250a2cd40297f7057a088861bb223a86f11055f
leonildomello42 <leonildo.mello@gmail.com>
2021-05-20 00:26:00 -0300

    Refatorando REST

---
+++ cap8_tratamento_modelagem_erros_api/aula6_Refatorando_API/algafood-api/src/main/java/com/algaworks/algafoodapi/domain/exception/EntidadeNaoEncontradaException.java
@@ -0,0 +1,15 @@
+package com.algaworks.algafoodapi.domain.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND) //, reason = "Entidade não encontrada")
+public class EntidadeNaoEncontradaException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public EntidadeNaoEncontradaException(String msg){
+        super(msg);
+    }
+}
