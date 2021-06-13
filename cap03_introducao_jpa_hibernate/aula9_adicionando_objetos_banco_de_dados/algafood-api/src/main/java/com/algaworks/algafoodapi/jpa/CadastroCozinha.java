c76ea7aa1af3817515c2e0c02bfd0d4b4c12dd9e
leonildomello42 <leonildo.mello@gmail.com>
2021-06-13 17:14:01 -0300

    Adicionando um objeto no banco de dados

---
+++ cap03_introducao_jpa_hibernate/aula9_adicionando_objetos_banco_de_dados/algafood-api/src/main/java/com/algaworks/algafoodapi/jpa/CadastroCozinha.java
@@ -0,0 +1,29 @@
+package com.algaworks.algafoodapi.jpa;
+
+import com.algaworks.algafoodapi.domain.model.Cozinha;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Component
+public class CadastroCozinha {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<Cozinha> listar(){
+        TypedQuery<Cozinha> query = entityManager.createQuery("FROM Cozinha", Cozinha.class);
+        return query.getResultList();
+    }
+
+    @Transactional
+    public Cozinha adicionar(Cozinha cozinha){
+
+        Cozinha cozinha1 = entityManager.merge(cozinha);
+        return cozinha1;
+    }
+}
---
+++ cap03_introducao_jpa_hibernate/aula9_adicionando_objetos_banco_de_dados/algafood-api/src/main/java/com/algaworks/algafoodapi/jpa/main/InclusaoCozinhaMain.java
@@ -0,0 +1,32 @@
+package com.algaworks.algafoodapi.jpa.main;
+
+import com.algaworks.algafoodapi.AlgafoodApiApplication;
+import com.algaworks.algafoodapi.domain.model.Cozinha;
+import com.algaworks.algafoodapi.jpa.CadastroCozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+public class InclusaoCozinhaMain {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+
+        Cozinha cozinha1 = new Cozinha();
+        cozinha1.setNome("Brasileira");
+
+        Cozinha cozinha2 = new Cozinha();
+        cozinha2.setNome("Japonesa");
+
+        cozinha1 = cadastroCozinha.adicionar(cozinha1);
+        cozinha2 = cadastroCozinha.adicionar(cozinha2);
+
+        System.out.printf("%d - %s\n", cozinha1.getId(), cozinha1.getNome());
+    }
+}
