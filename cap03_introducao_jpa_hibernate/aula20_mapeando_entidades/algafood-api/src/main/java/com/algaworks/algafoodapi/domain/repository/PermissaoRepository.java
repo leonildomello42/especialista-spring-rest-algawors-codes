4d8d439c0d08ed818d9df9150215c79a8db13734
leonildomello42 <leonildo.mello@gmail.com>
2021-06-17 00:44:37 -0300

    Desafio- implementando entidades

---
+++ cap03_introducao_jpa_hibernate/aula20_mapeando_entidades/algafood-api/src/main/java/com/algaworks/algafoodapi/domain/repository/PermissaoRepository.java
@@ -0,0 +1,12 @@
+package com.algaworks.algafoodapi.domain.repository;
+
+import com.algaworks.algafoodapi.domain.model.Permissao;
+
+import java.util.List;
+
+public interface PermissaoRepository {
+    List<Permissao> listar();
+    Permissao buscar(Long id);
+    Permissao salvar(Permissao permissao);
+    void remover(Permissao permissao);
+}
