1ebcafbed31ed2d738357d308d6f3595f83eb369
leonildomello42 <leonildo.mello@gmail.com>
2021-07-17 15:25:42 -0300

    Capitulo 1 introdução

---
+++ temp/v2/cap04_rest_com_spring/aula10_11_collectionResources_GET/algafood-api/src/main/java/com/algaworks/algafoodapi/domain/repository/FormaPagamentoRepository.java
@@ -0,0 +1,12 @@
+package com.algaworks.algafoodapi.domain.repository;
+
+import com.algaworks.algafoodapi.domain.model.FormaPagamento;
+
+import java.util.List;
+
+public interface FormaPagamentoRepository {
+    List<FormaPagamento> listar();
+    FormaPagamento buscar(Long id);
+    FormaPagamento salvar(FormaPagamento formaPagamento);
+    void remover(FormaPagamento formaPagamento);
+}
