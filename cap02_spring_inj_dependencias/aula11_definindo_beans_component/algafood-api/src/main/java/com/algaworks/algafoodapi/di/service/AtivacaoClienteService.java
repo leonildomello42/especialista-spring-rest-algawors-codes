6d5a676558c3bd11e675e7ab9a023b0b34550815
leonildomello42 <leonildo.mello@gmail.com>
2021-07-18 16:00:50 -0300

    Definindo Beans com Component

---
+++ cap02_spring_inj_dependencias/aula11_definindo_beans_component/algafood-api/src/main/java/com/algaworks/algafoodapi/di/service/AtivacaoClienteService.java
@@ -0,0 +1,18 @@
+package com.algaworks.algafoodapi.di.service;
+
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algafoodapi.di.modelo.Cliente;
+import com.algaworks.algafoodapi.di.notificacao.NotificadorEmail;
+
+@Component
+public class AtivacaoClienteService {
+	
+	private NotificadorEmail notificador;
+	
+	public void ativar(Cliente cliente) {
+
+		cliente.ativar();
+		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+	}
+}
