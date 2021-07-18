a2fff5dc0730ea5dbd2bdc7690e58a0a9f5c0465
leonildomello42 <leonildo.mello@gmail.com>
2021-07-18 18:00:03 -0300

    Conhecendo os pontos de injeção e a anotação Autowired

---
+++ cap02_spring_inj_dependencias/aula14_pontos_injecao_dependendcia_autowired/algafood-api/src/main/java/com/algaworks/algafoodapi/di/service/AtivacaoClienteService.java
@@ -0,0 +1,34 @@
+package com.algaworks.algafoodapi.di.service;
+
+import com.algaworks.algafoodapi.di.modelo.Cliente;
+import com.algaworks.algafoodapi.di.notificacao.Notificador;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AtivacaoClienteService {
+
+	@Autowired
+	private Notificador notificador;
+
+//	@Autowired private Notificador notificador;
+
+//	private @Autowired Notificador notificador;
+
+//	//Primeiro ponto de inj dep. no construtor
+//	@Autowired
+//	public AtivacaoClienteService(Notificador notificador) {
+//		this.notificador = notificador;
+//	}
+
+//	//Segundo ponto de inj dep. no set
+//	@Autowired
+//	public void setNotificador(Notificador notificador) {
+//		this.notificador = notificador;
+//	}
+
+	public void ativar(Cliente cliente) {
+		cliente.ativar();
+		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+	}
+}
