d9e4c7190136203974be1e4d79f7245fd6179c1b
leonildomello42 <leonildo.mello@gmail.com>
2021-07-18 18:08:39 -0300

    Dependência opcional com Autowired

---
+++ cap02_spring_inj_dependencias/aula15_dependendcia_opcional_autowired/algafood-api/src/main/java/com/algaworks/algafoodapi/di/service/AtivacaoClienteService.java
@@ -0,0 +1,25 @@
+package com.algaworks.algafoodapi.di.service;
+
+import com.algaworks.algafoodapi.di.modelo.Cliente;
+import com.algaworks.algafoodapi.di.notificacao.Notificador;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AtivacaoClienteService {
+
+	@Autowired(required = false)
+	private Notificador notificador;
+
+
+	public void ativar(Cliente cliente) {
+		cliente.ativar();
+
+		if(notificador != null){
+			notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+		}
+		else {
+			System.out.println("Não existe notificador , mas cliente foi ativado!");
+		}
+	}
+}
