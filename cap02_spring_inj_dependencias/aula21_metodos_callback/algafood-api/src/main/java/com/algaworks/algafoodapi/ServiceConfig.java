d45c651e981ff663813118fca79f29b33e4bf5ac
leonildomello42 <leonildo.mello@gmail.com>
2021-07-19 22:44:51 -0300

    Criando métodos de callback do ciclo de vida dos beans

---
+++ cap02_spring_inj_dependencias/aula21_metodos_callback/algafood-api/src/main/java/com/algaworks/algafoodapi/ServiceConfig.java
@@ -0,0 +1,15 @@
+package com.algaworks.algafoodapi;
+
+import com.algaworks.algafoodapi.di.service.AtivacaoClienteService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServiceConfig {
+
+
+    @Bean(initMethod = "init", destroyMethod = "destroy")
+    public AtivacaoClienteService ativacaoClienteService(){
+        return new AtivacaoClienteService();
+    }
+}
---
+++ cap02_spring_inj_dependencias/aula21_metodos_callback/algafood-api/src/main/java/com/algaworks/algafoodapi/di/notificacao/NotificadorEmail.java
@@ -0,0 +1,21 @@
+package com.algaworks.algafoodapi.di.notificacao;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algafoodapi.di.modelo.Cliente;
+
+@Profile("prod")
+@TipoDoNotificador(value = NivelUrgencia.URGENTE)
+@Component
+public class NotificadorEmail implements Notificador {
+
+	@Override
+	public void notificar(Cliente cliente, String mensagem) {
+
+		System.out.printf("Notificando %s através do e-mail %s: %s\n",
+				cliente.getNome(), cliente.getEmail(), mensagem);
+	}
+}
