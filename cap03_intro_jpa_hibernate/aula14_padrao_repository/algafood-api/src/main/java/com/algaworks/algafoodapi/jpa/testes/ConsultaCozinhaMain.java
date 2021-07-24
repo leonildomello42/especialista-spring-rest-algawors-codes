fe4796f587bf1235182e3224ce702576d14ec068
leonildomello42 <leonildo.mello@gmail.com>
2021-07-24 15:49:26 -0300

    Conhecendo e implementando o padrão Repository

---
+++ cap03_intro_jpa_hibernate/aula14_padrao_repository/algafood-api/src/main/java/com/algaworks/algafoodapi/jpa/testes/ConsultaCozinhaMain.java
@@ -0,0 +1,36 @@
+package com.algaworks.algafoodapi.jpa.testes;
+
+import com.algaworks.algafoodapi.AlgafoodApiApplication;
+import com.algaworks.algafoodapi.domain.model.Cozinha;
+import com.algaworks.algafoodapi.domain.repository.CozinhaRepository;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class ConsultaCozinhaMain {
+
+    public static void main(String[] args) {
+
+        ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CozinhaRepository cadastroCozinha =  applicationContext.getBean(CozinhaRepository.class);
+        List<Cozinha> cozinhas =  cadastroCozinha.listar();
+
+        for (Cozinha cozinha : cozinhas){
+            System.out.println(cozinha.getNome());
+        }
+
+        System.out.println("----------------------");
+
+        Iterator<Cozinha> iterator = cozinhas.iterator();
+
+        while (iterator.hasNext()){
+            System.out.println(iterator.next().getNome());
+        }
+    }
+}
