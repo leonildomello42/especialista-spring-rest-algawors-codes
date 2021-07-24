46df41e14bb10ad2e959a55c60bfc2ed5e4a38bd
leonildomello42 <leonildo.mello@gmail.com>
2021-07-24 15:09:44 -0300

    Atualizando um objeto no banco de dados

--- cap03_intro_jpa_hibernate/aula08_consultando_objetos_no_bd/algafood-api/src/main/java/com/algaworks/algafoodapi/jpa/testes/AlteracaoCozinhaMain.java
+++ cap03_intro_jpa_hibernate/aula08_consultando_objetos_no_bd/algafood-api/src/main/java/com/algaworks/algafoodapi/jpa/testes/AlteracaoCozinhaMain.java
@@ -7,7 +7,7 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 
-public class InclusaoCozinhaMain {
+public class AlteracaoCozinhaMain {
 
     public static void main(String[] args) {
 
@@ -17,17 +17,15 @@ public class InclusaoCozinhaMain {
 
         CadastroCozinha cadastroCozinha =  applicationContext.getBean(CadastroCozinha.class);
 
-        Cozinha cozinha1 = new Cozinha();
-        cozinha1.setNome("Brasilseira");
+        Cozinha cozinha = new Cozinha();
+        cozinha.setId(1L);
+        cozinha.setNome("Brasilseira");
 
-        Cozinha cozinha2 = new Cozinha();
-        cozinha2.setNome("Japonesa");
 
-        cozinha1 = cadastroCozinha.salvar(cozinha1);
-        cozinha2 = cadastroCozinha.salvar(cozinha2);
+        cozinha = cadastroCozinha.salvar(cozinha);
 
-        System.out.printf(" %d - %s\n", cozinha1.getId(), cozinha1.getNome());
-        System.out.printf(" %d - %s\n", cozinha2.getId(), cozinha2.getNome());
+        System.out.printf(" %d - %s\n", cozinha.getId(), cozinha.getNome());
+
 
     }
 }
