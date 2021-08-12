0c34de8ed0e1c902c469903507bce487f40ae689
leonildomello42 <leonildo.mello@gmail.com>
2021-08-12 00:54:13 -0300

    Configurando datasource

--- cap03_intro_jpa_hibernate/aula17_mapeando_relacionamentos_ManyToOne/algafood-api/src/main/resources/import.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO cozinha (id, nome) VALUES (1, 'Tailandesa');
-INSERT INTO cozinha (id, nome) VALUES (2, 'Indiana');
-
-INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('Thai Gourmet', 10, 1);
-INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('Thai Delivery', 9.50, 1);
-INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('Tuk Tuk Comida Indiana', 15, 2);
