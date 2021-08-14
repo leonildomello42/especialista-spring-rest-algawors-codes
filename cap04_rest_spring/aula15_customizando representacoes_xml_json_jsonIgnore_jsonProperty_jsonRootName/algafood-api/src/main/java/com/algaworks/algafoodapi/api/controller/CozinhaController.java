92ce4bda3b5b5db9e767a677b7321a3e9f6dc8e1
leonildomello42 <leonildo.mello@gmail.com>
2021-08-14 20:02:14 -0300

    Customizando as representações XML e JSON

---
+++ cap04_rest_spring/aula15_customizando representacoes_xml_json_jsonIgnore_jsonProperty_jsonRootName/algafood-api/src/main/java/com/algaworks/algafoodapi/api/controller/CozinhaController.java
@@ -0,0 +1,29 @@
+package com.algaworks.algafoodapi.api.controller;
+
+import com.algaworks.algafoodapi.domain.model.Cozinha;
+import com.algaworks.algafoodapi.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping
+    public List<Cozinha> listar(){
+        return cozinhaRepository.listar();
+    }
+
+    @GetMapping(value = "/{cozinhaId}")
+    public Cozinha buscar(@PathVariable("cozinhaId") Long id){
+        return cozinhaRepository.buscar(id);
+    }
+
+}
