222d28a3800da689817a10e6b202f3181e9b3526
leonildomello42 <leonildo.mello@gmail.com>
2021-08-15 18:13:52 -0300

    Modelando e implementando a inclusão de recursos com POST

---
+++ cap04_rest_spring/aula23_inclusao_post/algafood-api/src/main/java/com/algaworks/algafoodapi/jpa/testes/AlteracaoCozinhaMain.java
@@ -0,0 +1,31 @@
+package com.algaworks.algafoodapi.jpa.testes;
+
+import com.algaworks.algafoodapi.AlgafoodApiApplication;
+import com.algaworks.algafoodapi.domain.model.Cozinha;
+import com.algaworks.algafoodapi.domain.repository.CozinhaRepository;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+public class AlteracaoCozinhaMain {
+
+    public static void main(String[] args) {
+
+        ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CozinhaRepository cadastroCozinha =  applicationContext.getBean(CozinhaRepository.class);
+
+        Cozinha cozinha = new Cozinha();
+        cozinha.setId(1L);
+        cozinha.setNome("Brasilseira");
+
+
+        cozinha = cadastroCozinha.salvar(cozinha);
+
+        System.out.printf(" %d - %s\n", cozinha.getId(), cozinha.getNome());
+
+
+    }
+}
