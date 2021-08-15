0f6f9492b6e9fb4d78e489c77d6d98a8137d054a
leonildomello42 <leonildo.mello@gmail.com>
2021-08-15 17:32:47 -0300

    Definindo o status da resposta HTTP com @ResponseStatus

---
+++ cap04_rest_spring/aula19_definindo_status_htpp_responseStatus/algafood-api/src/main/java/com/algaworks/algafoodapi/api/controller/CozinhaController.java
@@ -0,0 +1,37 @@
+package com.algaworks.algafoodapi.api.controller;
+
+import com.algaworks.algafoodapi.api.model.CozinhasXmlWrapper;
+import com.algaworks.algafoodapi.domain.model.Cozinha;
+import com.algaworks.algafoodapi.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping
+    public List<Cozinha> listar(){
+        return cozinhaRepository.listar();
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+    public CozinhasXmlWrapper listarXml(){
+        return new CozinhasXmlWrapper(cozinhaRepository.listar());
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @GetMapping(value = "/{cozinhaId}")
+    public Cozinha buscar(@PathVariable("cozinhaId") Long id){
+        return cozinhaRepository.buscar(id);
+    }
+
+}
