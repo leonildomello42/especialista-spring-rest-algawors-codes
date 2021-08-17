0c59eb359423955a90ecea03f894bd9cf50a573a
leonildomello42 <leonildo.mello@gmail.com>
2021-08-17 02:14:24 -0300

    Criando consultas com query methods

---
+++ cap05_spring_data_jpa/aula06_consulta_query_methods/algafood-api/src/main/java/com/algaworks/algafoodapi/domain/repository/RestauranteRepository.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafoodapi.domain.repository;
+
+import com.algaworks.algafoodapi.domain.model.Cozinha;
+import com.algaworks.algafoodapi.domain.model.Restaurante;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+
+}
