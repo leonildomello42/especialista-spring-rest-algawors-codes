85c569a480fce6889869e8dc664c486821c4fd43
leonildomello42 <leonildo.mello@gmail.com>
2021-08-16 21:30:08 -0300

    Modelando e implementando a inclusão de recursos de restaurantes

---
+++ cap04_rest_spring/aula30_inclusao_recursos_restaurante/algafood-api/src/main/java/com/algaworks/algafoodapi/api/controller/RestauranteController.java
@@ -0,0 +1,52 @@
+package com.algaworks.algafoodapi.api.controller;
+
+import com.algaworks.algafoodapi.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafoodapi.domain.model.Restaurante;
+import com.algaworks.algafoodapi.domain.repository.RestauranteRepository;
+import com.algaworks.algafoodapi.domain.service.CadastroCozinhaService;
+import com.algaworks.algafoodapi.domain.service.CadastroRestauranteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/restaurantes")
+public class RestauranteController {
+
+    @Autowired
+    private RestauranteRepository restauranteRepository;
+
+    @Autowired
+    private CadastroRestauranteService cadastroRestaurante;
+
+    @GetMapping
+    public List<Restaurante> listar(){
+        return restauranteRepository.listar();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Restaurante> buscar(@PathVariable Long id){
+
+        Restaurante restaurante = restauranteRepository.buscar(id);
+
+        if(restaurante != null){
+            return ResponseEntity.ok(restaurante);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<?> salvar(@RequestBody Restaurante restaurante){
+
+        try{
+            restaurante = cadastroRestaurante.salvar(restaurante);
+            return ResponseEntity.status(HttpStatus.CREATED).body(restaurante);
+        }
+        catch (EntidadeNaoEncontradaException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
