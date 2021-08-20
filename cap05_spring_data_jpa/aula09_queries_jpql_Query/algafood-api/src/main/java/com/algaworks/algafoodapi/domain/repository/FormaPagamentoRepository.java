8192911c6c91ce08a456bc952bbdba7bcdcd1862
leonildomello42 <leonildo.mello@gmail.com>
2021-08-20 00:23:06 -0300

    Usando queries JPQL customizadas com @Query

---
+++ cap05_spring_data_jpa/aula09_queries_jpql_Query/algafood-api/src/main/java/com/algaworks/algafoodapi/domain/repository/FormaPagamentoRepository.java
@@ -0,0 +1,12 @@
+package com.algaworks.algafoodapi.domain.repository;
+
+import com.algaworks.algafoodapi.domain.model.FormaPagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, Long> {
+
+}
